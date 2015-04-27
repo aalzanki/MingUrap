d6e830aca9c7bbf44ff0eb7b022a31e6bfa8d906
Ethan Chiou <ethanchiou@berkeley.edu>
2015-04-27 00:19:43 -0700

    edit slider screen to enable offers.

--- MingUrap.spritebuilder/Source/MainScene.swift
+++ MingUrap.spritebuilder/Source/MainScene.swift
@@ -1,9 +1,14 @@
-import Foundation
+    import Foundation
 
 class MainScene: CCNode {
+    var percentSuccess = 50
+    var curGold = 2000.0
+    var reductionPerRound = 0.9
+    var gameOver = false
+    
     private func acceptBid(num : Float) -> Bool {
         println(num)
-        if (num > 50) {
+        if (num > Float(percentSuccess)) {
             return true
         } else {
             return false
@@ -11,13 +16,24 @@ class MainScene: CCNode {
     }
     
     func actionTapped() {
-        if let slider = getChildByName("slider", recursively: false) as? CCSlider {
-            var text = "Bid Accepted"
-            if (!acceptBid(slider.sliderValue * 100)) {
-                text = "Bid Denied"
-            }
-            if let title = getChildByName("title", recursively: false) as? CCLabelTTF {
-                title.string = text
+        if !gameOver || curGold < 1 {
+            if let slider = getChildByName("slider", recursively: false) as? CCSlider {
+                var text = "Bid Accepted"
+                var goldRemainingText = "You earned \(Int(slider.sliderValue * Float(curGold))) gold."
+                if (!acceptBid(slider.sliderValue * 100)) {
+                    text = "Bid Denied"
+                    curGold = curGold * reductionPerRound
+                    goldRemainingText = "Gold remaining: " + String(Int(curGold))
+                }
+                else {
+                    gameOver = true
+                }
+                if let title = getChildByName("title", recursively: false) as? CCLabelTTF {
+                    title.string = text
+                }
+                if let goldRemaining = getChildByName("goldRemaining", recursively: false) as? CCLabelTTF {
+                    goldRemaining.string = goldRemainingText
+                }
             }
         }
     }
